f2ae9f5f04b2353336f19884c836775bd9974d13
AnnKiende28 <155237311+AnnKiende28@users.noreply.github.com>
2024-03-26 17:43:53 +0300

    Update contact-us.php and samples.php
    Add links to other pages within the website

---
+++ contact-us.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us | AK Services</title>
+</head>
+<body>
+
+    <div class="nav-bar">
+        <a href="index.php">Home</a>
+        <a href="samples.php">Samples</a>
+    </div>
+    
+</body>
+</html>
